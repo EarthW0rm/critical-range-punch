6fab001ca1198239358400fd7a71ecad8209850c
EarthW0rm <marco.a.faustino@gmail.com>
2018-06-29 23:53:52 -0300

    Inclusao de um metodo de api para conversa entre containers

--- lion-o/routes/allys.js
+++ lion-o/routes/allys.js
@@ -1,10 +1,27 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
-
+var axios = require('axios');
 /* GET users listing. */
 router.get('/', function (req, res) {
     res.jsend.success({ allyName: 'Lion-O', allyWeapons: ['Sword of Omens', 'Claw Shield', 'Red Cloak']});
+});
+
+router.get('/:allyHost', async function (req, res) {
+    
+    var urlAlly = `http://${req.params.allyHost}/allys`;
+
+    try {       
+        var allyResponse = await axios.get(urlAlly);
+        res.jsend.success({allyResponse: allyResponse.data});
+
+    } catch(err) {
+        res.jsend.error({
+            code: 123,
+            message: `No access to ally: ${urlAlly} for you!`,
+            data: err
+        });
+    }
 });
 
 module.exports = router;
--- wilykit/routes/allys.js
+++ wilykit/routes/allys.js
@@ -7,4 +7,21 @@ router.get('/', function (req, res) {
     res.jsend.success({ allyName: 'Wilykit', allyWeapons: ['Lasso', 'Trick Pellets']});
 });
 
+router.get('/:allyHost', async function (req, res) {
+    
+    var urlAlly = `http://${req.params.allyHost}/allys`;
+
+    try {       
+        var allyResponse = await axios.get(urlAlly);
+        res.jsend.success({allyResponse: allyResponse.data});
+
+    } catch(err) {
+        res.jsend.error({
+            code: 123,
+            message: `No access to ally: ${urlAlly} for you!`,
+            data: err
+        });
+    }
+});
+
 module.exports = router;
