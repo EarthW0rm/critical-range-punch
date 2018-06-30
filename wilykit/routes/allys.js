d9e5ce3d94ae0f673be070981e1bc2e24ef38cda
EarthW0rm <marco.a.faustino@gmail.com>
2018-06-29 21:37:40 -0300

    App Basico Wilykit

---
+++ wilykit/routes/allys.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    res.jsend.success({ allyName: 'Wilykit', allyWeapons: ['Lasso', 'Trick Pellets']});
+});
+
+module.exports = router;
