ab4f457d132ebeb89c731048cc92388a0cb16e08
EarthW0rm <marco.a.faustino@gmail.com>
2018-06-29 21:08:28 -0300

    Criando a controller allys

--- panthro/app.js
+++ panthro/app.js
@@ -3,12 +3,12 @@ var debug = require('debug');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+var jsend = require('jsend');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var allys = require('./routes/allys');
 
 var app = express();
 
@@ -19,12 +19,13 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+app.use(jsend.middleware);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/allys', allys);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
