12b927f7a192e7cbcc5e5f19d3eb0157124f0be4
EarthW0rm <marco.a.faustino@gmail.com>
2018-06-29 21:36:53 -0300

    App Basico Jaga

---
+++ jaga/routes/allys.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    res.jsend.success({ allyName: 'Jaga', allyWeapons: ['Sword of Omens', 'Regular Sword']});
+});
+
+module.exports = router;
